40d6dfa76531f635a0e99ed1347397c89c1c8db9
Marcus Griffiths <marcusgriffiths@marcuss-macbook-pro.local>
2018-11-04 15:27:09 -0700

    Initial Commit

---
+++ Dobro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dobro
+//
+//  Created by Marcus Griffiths on 11/4/18.
+//  Copyright © 2018 Marcus Griffiths. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
