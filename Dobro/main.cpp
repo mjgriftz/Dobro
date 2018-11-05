a4254e9ea13e0f5976b7781247e01329c631ef79
Marcus Griffiths <marcusgriffiths@marcuss-macbook-pro.local>
2018-11-05 00:15:42 -0700

    Initial Commit

--- Dobro/main.cpp
+++ Dobro/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    // check filename
+    std::string target_file_name = argv[1];
+    std::ifstream target(target_file_name, std::ifstream::ate | std::ifstream::binary );
+    int target_size = (int)target.tellg();
+    std::cout << target_size << std::endl;
+    
+    // check sizes from root
+    
+    
+    // check data matches from size matches
+    // list true matches
     return 0;
 }
